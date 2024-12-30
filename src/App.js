bbde2e4bd638a965040f06a4d814046b6cb49ae8
Akshat Gupta <akshatgupta@Akshats-MacBook-Air.local>
2024-12-30 18:49:33 +0530

    new commmit - just creating the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Main from './Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>hello world</p>
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function Main(){
+    return (
+        <div>
+            <p>hello world</p>
+        </div>
+    )
+}
+
+export default Main;
